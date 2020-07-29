c394995b060a250793add154bf6c07a957805382
mjmiguel <migueljmiller@gmail.com>
2020-07-28 20:45:10 -0700

    add bcrypt and fix bugs

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,12 @@
 const db = require('../models/Models');
+const bcrypt = require('bcryptjs');
+const SALT_WORK_FACTOR = 10 // should be at least 1-
 
 const UserController = {};
 
 UserController.getUserItems = (req, res, next) => {
   // console.log(`req.params.id`, req.params.user);
+  console.log(req.body)
   const { user_id } = req.params;
   // console.log('user', user_id);
 
@@ -25,6 +28,7 @@ UserController.getUserItems = (req, res, next) => {
 };
 
 UserController.createUser = async (req, res, next) => {
+  console.log('req body in create ', req.body)
   const { email, password, firstName, lastName, zipCode, street, city, state } = req.body;
   try {
     // if user is in database, send res of user exists
@@ -40,19 +44,27 @@ UserController.createUser = async (req, res, next) => {
     };
     const address = await db.query(createAddressQuery);
 
-    // create user, use incoming address_id
-    const createUserQuery = {
-      text:
-        'INSERT INTO public.users("email", "firstName", "lastName", "password", "address_id") VALUES($1, $2, $3, $4, $5) RETURNING *',
-      values: [email, firstName, lastName, password, address.rows[0]._id],
-    };
+    // hash the provided password with brcrypt before insertion into db
+    await bcrypt.hash(password, SALT_WORK_FACTOR, async (err, hash) => {
+      if (err) return next(err);
+      let hashedPassword = hash;
 
-    const newUser = await db.query(createUserQuery);
+      // create user, use incoming address_id
+      const createUserQuery = {
+        text:
+          'INSERT INTO public.users("email", "firstName", "lastName", "password", "address_id") VALUES($1, $2, $3, $4, $5) RETURNING *',
+        values: [email, firstName, lastName, hashedPassword, address.rows[0]._id],
+      };
+  
+      let newUser = await db.query(createUserQuery, (err, res) => {
+        if (err) return next(err)
+        console.log('users query ', res)
+      });
+      // console.log('successfully create email', email);
+      // send data back to client
+      return next();
+    });
 
-    // console.log('successfully create email', email);
-    // send data back to client
-
-    return next();
     // res.status(200).json({ user: newUser.rows[0], adress: address.rows[0] });
 
     // res.redirect('/login')
@@ -64,57 +76,19 @@ UserController.createUser = async (req, res, next) => {
 };
 
 UserController.verifyUser = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { userEmail, password } = req.body;
+
   try {
-    const findUser = `SELECT _id, email, password FROM users WHERE (email = '${email}');`;
+    const findUser = `SELECT _id, email, password FROM users WHERE email = '${userEmail}';`;
     const user = await db.query(findUser);
-    if (password === user.rows[0].password) {
-      return next();
-    }
-    if (password !== user.rows[0].password) {
-      return next({ log: 'Incorrect password' });
-    }
+
+    await bcrypt.compare(password, user.rows[0].password, (err, result) => {
+      if (err) return next(err);
+      return (result ? next() : next({ log: 'Incorrect password' }))
+    });
   } catch (e) {
     return next({ log: 'Error returned, invalid username' });
   }
 };
 
 module.exports = UserController;
-//   // let {email, password, phone} = req.body
-//   console.log("req.body in create user", req.body);
-//   f (!username || pasowrd) return next (if username or
-// password feilds are missing)
-//   const phone = '+1' + req.body.phone;
-//   console.log(phone)
-//     User.create(req.body, (err, data) => {
-//       if (err) {
-//         // refactor to include error object as 2nd sargument to render
-//         return next({ log: 'Invalid create user query'});
-//       }
-//       client.messages
-//       .create({
-//           body: 'Hi, welcome to Graphick!',
-//           from: '+18052227105',
-//           to: phone,
-//         })
-//         .then((message) => console.log(message.sid));
-//       return next();
-//     }
-//   );
-//   };
-
-// implement functionality that checks for lacking data fields
-
-// User.findOne({ email }, (err, user) => {
-//   // console.log("user in verifyUser", user);
-//   if (err) return next({ log: 'Error returned, invalid username' });
-//   if (user === null) return next({ log: 'Cannot find user from username' });
-//   /** ------------------*BCRPYT - not using yet, so need to add functionality that mathes req email/pw to db email/pw----------------- */
-//   // if valid username, check password
-//   user.comparePassword(password, (err, isMatch) => {
-//     if (err) return next({log: 'Error occured while matching password via bcrypt'});
-//     if (isMatch === false) return next({log: 'Incorrect password - bcrypt match failed'})
-//     return next();
-
-//   // })
-// });
